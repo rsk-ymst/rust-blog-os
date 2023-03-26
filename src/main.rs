239ea90fa8d6ff667b0883c1e8d8620db76bb4c8
Yamashita <jey.togo@gmail.com>
2023-03-26 12:29:08 +0900

    [abort] doesnt work on win env

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@
 #![no_main]
 
 use core::panic::PanicInfo;
+mod vga_buffer;
 
 // エントリポイントを上書きしている
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
+    // let vga_buffer = 0xb8000 as *mut u8;
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    // for (i, &byte) in HELLO.iter().enumerate() {
+    //     unsafe {
+    //         *vga_buffer.offset(i as isize * 2) = byte;
+    //         *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    //     }
+    // }
+    vga_buffer::print_something();
 
 
     loop {}
