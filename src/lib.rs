ec637644aea4e0bdca8a7cdea04f5a4a97274309
rsk-ymst <jey.togo@gmail.com>
2023-04-10 20:08:50 +0900

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 
+use x86_64::instructions::hlt;
+
 pub mod serial;
 pub mod vga_buffer;
+mod interrupts;
+pub mod gdt;
+pub mod memory;
 
 pub trait Testable {
     fn run(&self) -> ();
@@ -36,7 +42,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    loop {}
+    hlt_loop();
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -55,16 +61,45 @@ pub fn exit_qemu(exit_code: QemuExitCode) {
     }
 }
 
-/// Entry point for `cargo xtest`
+/// Entry point for `cargo test`
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     test_panic_handler(info)
+}
+
+#[cfg(test)]
+use bootloader::{entry_point, BootInfo};
+
+#[cfg(test)]
+entry_point!(test_kernel_main);
+
+/// `cargo test`のエントリポイント
+#[cfg(test)]
+fn test_kernel_main(_boot_info: &'static BootInfo) -> ! {
+    // 前と同じ
+    init();
+    test_main();
+    hlt_loop();
+}
+
+pub fn init() {
+    gdt::init();
+    interrupts::init_idt();
+    unsafe { interrupts::PICS.lock().initialize(); }
+    x86_64::instructions::interrupts::enable();
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,103 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use blog_os::println;
+use blog_os::{println, init, hlt_loop};
+use x86_64::{VirtAddr, structures::paging::PageTable};
 use core::panic::PanicInfo;
+use bootloader::{BootInfo, entry_point, bootinfo};
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use blog_os::memory;
+    use blog_os::memory::BootInfoFrameAllocator;
+    use x86_64::{structures::paging::Page, VirtAddr}; // 新しいインポート
+
     println!("Hello World{}", "!");
+    blog_os::init();
+
+    // invoke a breakpoint exception
+    // x86_64::instructions::interrupts::int3(); // new
+
+    // loop {
+    //     use blog_os::print;
+    //     print!("-")
+    // }
+
+    let mut frame_allocator = unsafe {
+        BootInfoFrameAllocator::init(&boot_info.memory_map)
+    };
+
+    // use x86_64::registers::control::Cr3;
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = memory::EmptyFrameAllocator;
+
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+    // let addresses = [
+    //     // 恒等対応しているVGAバッファのページ
+    //     0xb8000,
+    //     // コードページのどこか
+    //     0x201008,
+    //     // スタックページのどこか
+    //     0x0100_0020_1a10,
+    //     // 物理アドレス "0" にマップされている仮想アドレス
+    //     boot_info.physical_memory_offset,
+    // ];
+
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     // 追加：`mapper.translate_addr`メソッドを使う
+    //     let phys = mapper.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
+
+    // let (level_4_page_table, _) = Cr3::read();
+    // println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
 
     #[cfg(test)]
     test_main();
-
-    loop {}
+    
+    println!("It dit not crash!");
+    hlt_loop();
 }
+
+// #[no_mangle]
+// pub extern "C" fn _start(boot_info: &'static BootInfo) -> ! {
+//     println!("Hello World{}", "!");
+//     blog_os::init();
+
+//     // invoke a breakpoint exception
+//     // x86_64::instructions::interrupts::int3(); // new
+
+//     // loop {
+//     //     use blog_os::print;
+//     //     print!("-")
+//     // }
+//     println!("It dit not crash!");
+
+//     use x86_64::registers::control::Cr3;
+
+//     let (level_4_page_table, _) = Cr3::read();
+//     println!("Level 4 page table at: {:?}", level_4_page_table.start_address());
+
+//     #[cfg(test)]
+//     test_main();
+
+//     hlt_loop();
+// }
 
 /// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    blog_os::hlt_loop();
 }
 
 #[cfg(test)]
